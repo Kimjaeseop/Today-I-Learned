fcbd8e5e0190d170ee5387b6863456539bea6db6
jaeseb96@naver.com <jaeseb96@naver.com>
2021-06-09 22:03:33 +0900

    add project 'I am Rich'

---
+++ SWIFT/Angela-Yu-iOS-Lecture/I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by JaeseopKim on 2021/06/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- SWIFT/README.md
+++ SWIFT/README.md
@@ -4,5 +4,4 @@
 - 1차 목표 인스타그램 클론 코딩
 
 ## Angela Yu iOS Lecture
-- [Section 1](https://github.com/Kimjaeseop/Today-I-Learned/blob/main/SWIFT/Angela-Yu-iOS-Lecture/Section_1.md)
 ## Study
